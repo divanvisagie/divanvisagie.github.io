4c6c71dcf05a0b261b0bff9cd90a24d4d464bd1a
Divan Visagie <me@divanv.com>
2024-07-09 23:38:37 +0200

    Update js

--- js/index.js
+++ js/index.js
@@ -31,21 +31,21 @@ function toggleDarkMode() {
 }
 
 // https://stackoverflow.com/questions/56300132/how-to-over-ride-css-prefers-color-scheme-setting
-//determines if the user has a set theme
+// determines if the user has a set theme
 function detectColorScheme() {
     var theme = "light";    //default to light
 
     //local storage is used to override OS theme settings
     if (localStorage.getItem("theme")) {
         if (localStorage.getItem("theme") == "dark") {
-            var theme = "dark";
+            theme = "dark";
         }
     } else if (!window.matchMedia) {
         //matchMedia method not supported
         return false;
     } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
         //OS theme setting detected as dark
-        var theme = "dark";
+        theme = "dark";
     }
 
     //dark theme preferred, set document with a `data-theme` attribute
