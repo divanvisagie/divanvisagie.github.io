e1deea3ee81d795f9ca03e4c264729dd96bb11ec
Divan Visagie <me@divanv.com>
2025-12-22 17:42:52 +0100

    Deploy via gh 2025-12-22T16:42:52Z

--- js/index.js
+++ js/index.js
@@ -1,54 +1,5 @@
-
-// deno-lint-ignore-file
-var mediaQuery = window.matchMedia ? window.matchMedia('(prefers-color-scheme: dark)') : null
-
-function applyTheme(theme) {
-    document.documentElement.setAttribute('data-theme', theme)
-}
-
-function getTextFor(theme) {
-    if (theme === 'dark')
-        return '☽'
-    return '☼';
-}
-
-function getOpposite(theme) {
-    if (theme === 'dark') return 'light'
-    return 'dark'
-}
-
-function getCurrentTheme() {
-    var dataTheme = document.documentElement.getAttribute('data-theme');
-    if (!dataTheme) {
-        dataTheme = 'light'
-    }
-    return dataTheme
-}
-
-// https://stackoverflow.com/questions/56300132/how-to-over-ride-css-prefers-color-scheme-setting
-// determines if the user has a set theme
-function detectColorScheme() {
-    var theme = "light";    //default to light
-
-    if (mediaQuery && mediaQuery.matches) {
-        //OS theme setting detected as dark
-        theme = "dark";
-    }
-
-    applyTheme(theme)
-}
-detectColorScheme();
+// Add copy buttons to code blocks
 (function () {
-    detectColorScheme();
-
-    if (mediaQuery) {
-        mediaQuery.addEventListener('change', function (e) {
-            var theme = e.matches ? "dark" : "light";
-            applyTheme(theme)
-        })
-    }
-
-    // Add copy buttons to code blocks
     document.querySelectorAll('pre').forEach(function (pre) {
         var button = document.createElement('button');
         button.className = 'copy-button';
